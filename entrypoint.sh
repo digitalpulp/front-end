5a4d66ecaca402012fee06069340140e796ce9e1
Shawn Duncan <sduncan@digitalpulp.com>
2017-09-05 14:36:53 -0400

    Moving bower components to composer.json

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,11 +5,5 @@
 if ! [ -n ${THEME_NAME} -a -h /var/www/docroot/themes/custom/${THEME_NAME}/node_modules ]; then
     ln -s -f /var/www/front_end/node_modules /var/www/docroot/themes/custom/${THEME_NAME}/node_modules
 fi
-# If the theme path is set, force copy the bower_components into the theme.
-# Unlike the node_modules that are only used in compilation, the bower_components
-# need to be deployed with the site.
-if [ -n ${THEME_NAME} -a -d /var/www/docroot/themes/custom/${THEME_NAME} -a -d /var/www/front_end/bower_components ]; then
-    mv -f /var/www/front_end/bower_components /var/www/docroot/themes/custom/${THEME_NAME}/bower_components
-fi
-
+# Keep the container present.
 tail -f /dev/null
