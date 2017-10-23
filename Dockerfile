94d5469b03be541c1cc3de1ea960c35d8a78ab77
Shawn Duncan <sduncan@digitalpulp.com>
2017-10-23 12:08:54 -0400

    Bump to node 6.11.4

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,9 @@
-FROM mhart/alpine-node:4.8.1
+FROM mhart/alpine-node:6.11.4
 
 VOLUME /var/www
 
 RUN addgroup -g 82 -S www-data \
   && adduser -u 82 -D -S -G www-data www-data \
-  && npm install --global npm@2.15.11 \
   && apk add --no-cache make gcc g++ python git
 
 COPY entrypoint.sh /usr/local/bin/
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 # If theme name is set and a package.json is present,
 # build the node modules.
-if ! [ -n ${THEME_NAME} -a -r /var/www/docroot/themes/custom/${THEME_NAME}/package.json ]; then
+if [ -n ${THEME_NAME} -a -r /var/www/docroot/themes/custom/${THEME_NAME}/package.json ]; then
     npm install
 fi
 # Keep the container present.
