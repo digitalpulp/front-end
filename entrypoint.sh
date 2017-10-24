1fe301f5cde7f7774be752b1129326470a69d7b2
Shawn Duncan <sduncan@digitalpulp.com>
2017-10-24 13:36:38 -0400

    Also compile the theme.

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,11 @@
 if [ -n ${THEME_NAME} -a -r /var/www/docroot/themes/custom/${THEME_NAME}/package.json ]; then
     npm install
 fi
+# compile the theme.
+if [ -n ${THEME_NAME} -a -r /var/www/docroot/themes/custom/${THEME_NAME}/gulpfile.js ]; then
+    if node_modules/.bin/gulp build ; then
+      touch COMPILED.txt
+    fi
+fi
 # Keep the container present.
 tail -f /dev/null
