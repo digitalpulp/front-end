f2e6f46f0954a3aaf5badab0efc2c2aa50e122d0
Shawn Duncan <sduncan@digitalpulp.com>
2017-10-27 17:00:07 -0400

    Add additional flags.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,12 @@
 # If theme name is set and a package.json is present,
 # build the node modules.
 if [ -n ${THEME_NAME} -a -r /var/www/docroot/themes/custom/${THEME_NAME}/package.json ]; then
+    touch BUILDING.txt
     npm install
 fi
 # compile the theme.
 if [ -n ${THEME_NAME} -a -r /var/www/docroot/themes/custom/${THEME_NAME}/gulpfile.js ]; then
+    touch COMPILING.TXT
     node_modules/.bin/gulp build
     touch INITIALIZED.txt
 fi
