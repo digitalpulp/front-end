1f8f230ef84fb8d3db91dd2af74c5bff11607f0e
Shawn Duncan <sduncan@digitalpulp.com>
2017-12-06 16:35:12 -0500

    Make the path more configurable.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,21 @@
 #! /bin/sh
 
+WORKING_DIR="/var/www/docroot/themes/custom"
+if [ -n ${THEME_PATH} ]; then
+    WORKING_DIR=${THEME_PATH}
+fi
 # If theme name is set and a package.json is present,
 # build the node modules.
-if [ -n ${THEME_NAME} -a -r /var/www/docroot/themes/custom/${THEME_NAME}/package.json ]; then
+if [ -n ${THEME_NAME} -a -r ${WORKING_DIR}/${THEME_NAME}/package.json ]; then
     touch BUILDING.txt
     npm install
 fi
 # compile the theme.
-if [ -n ${THEME_NAME} -a -r /var/www/docroot/themes/custom/${THEME_NAME}/gulpfile.js ]; then
+if [ -n ${THEME_NAME} -a -r ${WORKING_DIR}/${THEME_NAME}/gulpfile.js ]; then
     touch COMPILING.txt
     node_modules/.bin/gulp build
     touch INITIALIZED.txt
-elif [ -n ${THEME_NAME} -a -r /var/www/docroot/themes/custom/${THEME_NAME}/Gruntfile.js ]; then
+elif [ -n ${THEME_NAME} -a -r ${WORKING_DIR}/${THEME_NAME}/Gruntfile.js ]; then
     touch COMPILING.txt
     export GRUNT_TARGET=dist
     node_modules/.bin/grunt build
