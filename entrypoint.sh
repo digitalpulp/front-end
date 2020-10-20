61c7d0518eac87de569f8fa6e8ebe96f6213bf93
Sam Thompson <sthompson@digitalpulp.com>
2020-10-20 11:48:45 -0400

    Fixes for ballast environment

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,14 +4,16 @@ set -e
 
 WORKING_DIR=${THEME_PATH:-"/var/www/docroot/themes/custom"}
 
-[ "${THEME_NAME}" = "" ] && echo "Error, THEME_NAME variable not specified" && exit 1
+[ "${THEME_NAME}" = "" ] && echo "Error, THEME_NAME variable not specified. Exiting front-end." && exit 1
 
-THEME_DIR = "${WORKING_DIR}/${THEME_NAME}"
+THEME_DIR="${WORKING_DIR}/${THEME_NAME}"
 
 [ ! -d "$THEME_DIR" ] && mkdir -p "$THEME_DIR"
 
 # Execute node-build "nodenv install" command from the theme directory.
-cd ${THEME_DIR} && nodenv install
+# Pass the "-s" flag to skip installing versions that already exist in 
+# the base image.
+cd ${THEME_DIR} && nodenv install -s || true
 
 # Keep the container present if not in a CI, otherwise exit.
 if [ -z ${CI_BUILD_ID} ]; then
