56fb2f7cbc412552368153a9c0d05f2cea0d7a6e
Shawn Duncan <sduncan@digitalpulp.com>
2021-05-03 16:14:13 -0400

    Release 2.0.4 Adjust entrypoint.sh to allow pass through commands in CI/CD

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,24 @@
 #! /bin/sh
 
-set -e
-
-WORKING_DIR=${THEME_PATH:-"/var/www/docroot/themes/custom"}
-
-[ "${THEME_NAME}" = "" ] && echo "Error, THEME_NAME variable not specified. Exiting front-end." && exit 1
-
-THEME_DIR="${WORKING_DIR}/${THEME_NAME}"
-
-[ ! -f "$THEME_DIR/.node-version" ] && echo "Error, .node-version file not present in theme directory ${THEME_DIR}. Exiting front-end." && exit 1
-
-# Execute node-build "nodenv install" command from the theme directory.
-# Pass the "-s" flag to skip installing versions that already exist in
-# the base image.
-cd ${THEME_DIR} && nodenv install -s || true
-
-# Keep the container present if not in a CI, otherwise exit.
 if [ -z ${CI_BUILD_ID} ]; then
+  set -e
+
+  WORKING_DIR=${THEME_PATH:-"/var/www/docroot/themes/custom"}
+
+  [ "${THEME_NAME}" = "" ] && echo "Error, THEME_NAME variable not specified. Exiting front-end." && exit 1
+
+  THEME_DIR="${WORKING_DIR}/${THEME_NAME}"
+
+  [ ! -f "$THEME_DIR/.node-version" ] && echo "Error, .node-version file not present in theme directory ${THEME_DIR}. Exiting front-end." && exit 1
+
+  # Execute node-build "nodenv install" command from the theme directory.
+  # Pass the "-s" flag to skip installing versions that already exist in
+  # the base image.
+  cd ${THEME_DIR} && nodenv install -s || true
+
+  # Keep the container present.
   tail -f /dev/null
+else
+  # Running in CI - just execute the given command or script.
+  exec "$@"
 fi
